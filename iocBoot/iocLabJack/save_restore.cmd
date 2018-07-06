cab3d7f3419e6c3c798b7951c8b0d7a99beb4173
Kurt A. Goetze <goetze@conifer.aps.anl.gov>
2018-07-06 15:34:01 -0500

    Initial commit of module contents

---
+++ iocBoot/iocLabJack/save_restore.cmd
@@ -0,0 +1,29 @@
+# Debug-output level
+save_restoreSet_Debug(0)
+
+# Set PV prefix
+save_restoreSet_status_prefix("xyz:")
+
+# Ok to save/restore save sets with missing values (no CA connection to PV)?
+save_restoreSet_IncompleteSetsOk(1)
+
+# Save dated backup files?
+save_restoreSet_DatedBackupFiles(1)
+
+# Number of sequenced backup files to write
+save_restoreSet_NumSeqFiles(3)
+
+# Time interval between sequenced backups
+save_restoreSet_SeqPeriodInSeconds(300)
+
+# Specify directories to search for save files
+set_savefile_path(".","autosave")
+
+# Specify directories to search for request files
+set_requestfile_path(".",  "")
+set_requestfile_path(".",  "autosave")
+set_requestfile_path($(AUTOSAVE),"asApp/Db")
+set_requestfile_path($(TOP), "LabJackApp/Db")
+
+# Load database
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=xyz:")
