821b66f565bb50d228df32e57fe3142a236bbf78
Mark Rivers <rivers@cars.uchicago.edu>
2022-10-11 14:14:40 -0500

    New file to generate LabJack_T7_[2,3].substititions from LabJack_T7_1.substitutions

---
+++ iocBoot/iocLabJack/generate_T7_2_and_3_substitutions.sh
@@ -0,0 +1,6 @@
+cp LabJack_T7_1.substitutions LabJack_T7_2.substitutions
+cp LabJack_T7_1.substitutions LabJack_T7_3.substitutions
+sed -i s/:1:/:2:/ LabJack_T7_2.substitutions
+sed -i s/_1_/_2_/ LabJack_T7_2.substitutions
+sed -i s/:1:/:3:/ LabJack_T7_3.substitutions
+sed -i s/_1_/_4_/ LabJack_T7_3.substitutions
