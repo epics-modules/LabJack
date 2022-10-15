291b211cd3bb9efee8732e93d871e9702b7b9226
Mark Rivers <rivers@cars.uchicago.edu>
2022-10-15 08:04:08 -0500

    Fixed typo

--- iocBoot/iocLabJack/generate_T7_2_and_3_substitutions.sh
+++ iocBoot/iocLabJack/generate_T7_2_and_3_substitutions.sh
@@ -3,4 +3,4 @@ cp LabJack_T7_1.substitutions LabJack_T7_3.substitutions
 sed -i s/:1:/:2:/ LabJack_T7_2.substitutions
 sed -i s/_1_/_2_/ LabJack_T7_2.substitutions
 sed -i s/:1:/:3:/ LabJack_T7_3.substitutions
-sed -i s/_1_/_4_/ LabJack_T7_3.substitutions
+sed -i s/_1_/_3_/ LabJack_T7_3.substitutions
