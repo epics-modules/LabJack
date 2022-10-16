05f0d90d4bf9df8768d4b662898e930e959d31f1
Mark Rivers <rivers@cars.uchicago.edu>
2022-10-16 12:27:25 -0500

    Enhancements and simplifications (#3)
    * New versions
    
    * New file
    
    * New generic file, replaces LabJack_T7_[1,2,3].cmd
    
    * New file to generate LabJack_T7_[2,3].substititions from LabJack_T7_1.substitutions
    
    * Obsolete, replaced by LabJack_T7.cmd
    
    * New version; simplified with fewer asynPortDrivers, other changes
    
    * Use environment variables to have a single .cmd file rather than 3
    
    * Enable T7_1 and T7_2
    
    * New file
    
    * New file, replaces LabJack_T7_seDiff.db
    
    * New file
    
    * Install .substitutions files as well
    
    * Replaced with LabJack_T7_seDiff.template
    
    * Add AIRange and AIResolution; remove global seDiff record
    
    * Replaced with LabJack_T7_AiConfig.adl
    
    * New file, replaces LabJack_T7_AiSetup.adl
    
    * Replace LabJack_T7_AiSetup.adl with LabJack_T7_AiConfig.adl
    
    * Remove DATA_TYPE macro, not needed, set when creating Modbus driver
    
    * Remove DATA_TYPE macro, not needed, set when creating Modbus driver
    
    * Remove tab characters
    
    * Use symbolic names for data types; remove unneeded Ao drivers, just use one for primary and one for LJTick
    
    * Remove DATA_TYPE macro for Ai and Ao, driver sets it; eliminate multiple Ao drivers
    
    * Remove DATA_TYPE macro for Ai and Ao, driver sets it; eliminate multiple Ao drivers
    
    * Remove DATA_TYPE macro for Ai and Ao, driver sets it; eliminate multiple Ao drivers
    
    * Improve white space
    
    * Replaces LabJack_T4_1.cmd
    
    * Replaced by LabJack_T4.cmd
    
    * Obsolete
    
    * Renamed
    
    * Renamed from Labjack_T7_bi_bit.template
    
    * Renamed
    
    * New autoconverted file
    
    * New file for autoconvert
    
    * New autoconvert version
    
    * Pull in files from SUPPORT
    
    * Hardcode I/O Intr
    
    * Add longin records for digital I/O; reformat screen to save space
    
    * Changed template file names; changed how digital inputs are done
    
    * Changed template file names; changed how digital inputs are done
    
    * Obsolete
    
    * Fix comment
    
    * Fix formatting and typos in comments
    
    * Many changes to be like LabJack_T7.cmd, many fewer asyn port drivers
    
    * Many changes to be like LabJack_T7.cmd, many fewer asyn port drivers
    
    * Renamed LabJack_T7_aiConfig.adl back to  LabJack_T7_aiSetup.adl for backwards compat.
    
    * Renamed LabJack_T7_aiConfig.adl back to  LabJack_T7_aiSetup.adl for backwards compat.
    
    * New file
    
    * New autoconvert
    
    * New file
    
    * Obsolete
    
    * Add Resolution
    
    * Add longin records for binary inputs
    
    * Fixed typo
    
    * Use iocshLoad rather than setting environment variables
    
    * Fix error in OFFSET for AIResolution
    
    * More notes for R2-0

---
+++ RELEASE.md
@@ -0,0 +1,18 @@
+# labjack Release Notes
+
+## R2-0 (October XXX, 2022)
+Many changes to simplify and add new features.
+- Added support for the analog input Range feature to select the voltage range.
+- Added support for the analog input Resolution feature to control resolution, allowing decreased noise at the expense of speed.
+- Added control for Range and Resolution on LabJack_T7_AiSetup.adl.
+- Added LabJack_T4_AiSetup.adl to control Resolution on the T4.  Range is not available on the T4.
+- Decreased the number drvModbusAsyn port drivers created for the T7 from 25 to 8, and for the T4 from 19 to 6.
+- Replaced multiple startup scripts (LabJack_T7_1.cmd, LabJack_T7_2.cmd, etc.) with a single LabJack_T7.cmd using environment variables.
+- Added generate_T7_2_and_3.subsititions.sh script that generates the subsititions files for modules 2 and 3 from LabJack_T7_1.substitutions.
+- Changed all .template files to be model-independent.
+- Removed the DATA_TYPE macro for all .template files. It is specified in the drvModbusAsynConfigure command for that driver.
+- Changed the bi records from using calcout records to select the bit, to using the mask parameter in asynUInt32Digital.
+- Added longin records for the FIO, CIO, EIO, and MIO binary inputs to the main T7 and T4 screens. 
+
+## R1-0 (October 14, 2022)
+- Initial release of labjack module.
---
+++ iocBoot/iocLabJack/LabJack_T7.cmd
@@ -0,0 +1,57 @@
+# LabJack_T7.cmd
+
+# The following environment variables must be set before loading this file
+
+#          P : The PV prefix for this IOC
+# IP_ADDRESS : The IP address or name of this Labjack
+#          N : The unit number (1 for first, 2 for second, etc.)
+
+# Use the following commands for TCP/IP
+#drvAsynIPPortConfigure(const char *portName,
+#                       const char *hostInfo,
+#                       unsigned int priority,
+#                       int noAutoConnect,
+#                       int noProcessEos);
+#
+drvAsynIPPortConfigure("LJT7_$(N)","$(IP_ADDRESS):502",0,0,0)
+
+#modbusInterposeConfig(const char *portName,
+#                      modbusLinkType linkType,
+#                      int timeoutMsec,
+#                      int writeDelayMsec)
+# One per controller.  One controller can support up to six drivers.
+modbusInterposeConfig("LJT7_$(N)",0,2000,0)
+
+# Modbus standard functions 3 (Read Multiple), 4 (Read One), 6 (Write One), and 16 (Write Multiple).
+# For writing either function code=6 (single register) or 16 (multiple registers) can be used, but 16
+# is better because it is "atomic" when writing values longer than 16-bits.
+
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+
+#                           portName             TCPName      Slave Function Address Length  Datatype    Poll   PLCType
+# Read digital inputs as words (FIO, CIO, EIO, MIO)
+drvModbusAsynConfigure("LJT7_$(N)_DIO_In",       "LJT7_$(N)",   1,     3,     2500,    4,        UINT16,  100, "LJT7_module")
+
+# Set digital outputs one bit at a time (FIO, CIO, EIO, MIO)
+# DIO8 - DIO15 (EIO bank) 
+drvModbusAsynConfigure("LJT7_$(N)_DIO_Out",      "LJT7_$(N)",   0,     6,     2000,   23,        UINT16,    1, "LJT7_module")
+
+# Read 14 analog inputs.
+drvModbusAsynConfigure("LJT7_$(N)_AI_In",        "LJT7_$(N)",   1,     3,        0,   28,    FLOAT32_BE,  100, "LJT7_module")
+
+# Write to 14 registers to configure analog range.
+drvModbusAsynConfigure("LJT7_$(N)_AIRange",      "LJT7_$(N)",   0,    16,    40000,   28,    FLOAT32_BE,    1, "LJT7_module")
+
+# Write to register to set each even no. AI channel Single-Ended or Differential
+drvModbusAsynConfigure("LJT7_$(N)_AIDiff",       "LJT7_$(N)",   0,     6,    41000,   13,        UINT16,    1, "LJT7_module")
+
+# Write to 14 registers to configure analog resolution index.
+drvModbusAsynConfigure("LJT7_$(N)_AIResolution", "LJT7_$(N)",   0,     6,    41500,   14,        UINT16,    1, "LJT7_module")
+
+# Set 2 primary analog outputs.
+drvModbusAsynConfigure("LJT7_$(N)_AO",           "LJT7_$(N)",   0,    16,     1000,    4,    FLOAT32_BE,    1, "LJT7_module")
+
+# Additional analog outputs using LJTick-DAC occupying FIO, EIO, CIO
+drvModbusAsynConfigure("LJT7_$(N)_AO_TICK",      "LJT7_$(N)",   0,    16,    30000,   40,     FLOAT32_BE,   0, "LJT7_module")
+
+dbLoadTemplate("LabJack_T7_$(N).substitutions", "P=$(P)")
