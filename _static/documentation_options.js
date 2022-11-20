1a7b3268100ca19a6357e1c57a2b0ce4782d321e
GitHub Action <action@github.com>
2022-11-20 23:40:58 +0000

    Add changes for d7528f1a8555886a68095c4bcc49d144c8102958

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2-0-70-ge15047d',
+    VERSION: '2-0-76-gd7528f1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
