087cfd9a09e01bd3c6dbe56a250fb0a8743f893f
GitHub Action <action@github.com>
2022-11-21 21:52:58 +0000

    Add changes for 386cb11b392ac7ed43739967490936ed20767f98

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2-0-76-gd7528f1',
+    VERSION: '2-0-79-g386cb11',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
